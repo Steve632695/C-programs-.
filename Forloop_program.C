1e4019e11a7ecacba36be2dc7c5b19bdcfeb7ed6
Steve632695 <stephenmambo272@gmail.com>
2025-03-10 13:22:13 +0300

    Add files via upload

---
+++ Forloop_program.C
@@ -0,0 +1,19 @@
+//program to calculate cube of numbers within a cerntain range.
+//stephen Mambo
+//D33-2421-2023
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    
+    // Prompt user for input
+    printf("Input number of terms: ");
+    scanf("%d", &n);
+
+    // Loop to print numbers and their cubes
+    for (i = 1; i <= n; i++) {
+        printf("Number is: %d and cube of the %d is: %d\n", i, i, i * i * i);
+    }
+
+    return 0;//program executed successfuly
+}
